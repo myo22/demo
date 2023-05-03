29d790e0875d54d65520c90a2273afbc03ccb2d0
myo22 <dlalsgud12@naver.com>
2023-05-03 14:57:42 +0900

    Spring Boot

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+@SpringBootApplication // 설정파일이면서도 컨퍼런스이다.
+public class DemoApplication { // 고로 DemoApplication 클래스 자체가 설정파일이다.
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
